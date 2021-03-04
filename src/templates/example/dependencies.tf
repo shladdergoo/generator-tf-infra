761d735f4f93097cc3ba58b20fe1c5a6d3cde802
shladdergoo <shladdergoo@users.noreply.github.com>
2021-03-04 07:30:21 +0000

    comment better epxlain example dependencies

--- src/templates/example/dependencies.tf
+++ src/templates/example/dependencies.tf
@@ -1,3 +1,13 @@
 # If the module being created in this example has any dependencies (resources 
 # that it requires in ALL cases), then describe them here so that the example 
 # can be deployed.
+
+# example
+
+# data "aws_vpc" "default" {
+#   default = true
+# }
+
+# data "aws_subnet_ids" "default" {
+#   vpc_id = data.aws_vpc.default.id
+# }
