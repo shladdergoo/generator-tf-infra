bd9b949819c56f53942535f71feb2b45db575415
shladdergoo <shladdergoo@users.noreply.github.com>
2021-06-17 14:06:25 +0100

    refactor ahead of adding CGP version

---
+++ src/generators/app/defaults.ts
@@ -0,0 +1,25 @@
+export class Defaults {
+  public static providers = [{ provider: 'aws', defaultRegion: 'us-east-1' }];
+  public static envListDefault = ['dev', 'staging', 'prod'];
+  public static stateBucketDefault = 'terraform-state';
+  public static stateKeyDefault = 'terraform.tfstate';
+  public static lockTableDefault = 'terraform-locks';
+  public static sampleModuleDefault = false;
+  public static sampleModuleName = 'vpc';
+  public static dirEnvironments = 'environments';
+  public static dirModules = 'modules';
+  public static dirExamples = 'examples';
+  public static dirTests = 'tests';
+  public static fileMain = 'main.tf';
+  public static fileVariables = 'variables.tf';
+  public static fileOutputs = 'outputs.tf';
+  public static fileDependencies = 'dependencies.tf';
+  public static statciFiles = [
+    ['dotgitignore', '.gitignore'],
+    ['dotgitattributes', '.gitattributes'],
+  ];
+
+  public static GetDefaultRegion(provider: string): string | undefined {
+    return Defaults.providers.find(p => p.provider === provider)?.defaultRegion;
+  }
+}
